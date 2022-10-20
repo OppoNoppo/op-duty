0fd96bc5f61f5f2c823103565562591d76d8d820
OppoNoppo <45967788+OppoNoppo@users.noreply.github.com>
2022-10-20 21:40:55 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,16 +3,22 @@ Config.dutyJobs = {
         ['police'] = {
             onDuty = 'police',
             offDuty = 'offpolice',
-            coords = vector3(441.81, -982.05, 30.69)
+            coords = {
+                vector3(441.81, -982.05, 30.69),
+            }
         },
         ['ambulance'] = {
             onDuty = 'ambulance',
             offDuty = 'offambulance',
-            coords = vector3(312.2638, -592.8132, 43.2821)
+            coords = {
+                vector3(312.2638, -592.8132, 43.2821),
+            }
         },
         ['mechanic'] = {
             onDuty = 'mechanic',
             offDuty = 'offmechanic',
-            coords = vector3(-347.5384, -133.1868, 39.0022)
+            coords = {
+                vector3(-347.5384, -133.1868, 39.0022),
+            }
         }
 }
